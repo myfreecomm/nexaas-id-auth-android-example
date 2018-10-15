aea7996224d951e46cb7ecaac64e713347354b5d
lhalegria <luizh.alegria@gmail.com>
2018-10-15 19:55:11 -0300

    Migrate strings from res to Consts file

--- nexaasidauth/src/main/java/com/nexaas/android/nexaasidauth/helper/Utils.kt
+++ nexaasidauth/src/main/java/com/nexaas/android/nexaasidauth/helper/Utils.kt
@@ -1,17 +1,13 @@
 package com.nexaas.android.nexaasidauth.helper
 
-import com.nexaas.android.nexaasidauth.NexxasIdAuthApp
-import com.nexaas.android.nexaasidauth.R
 import java.net.SocketTimeoutException
 
 object Utils {
 
-    fun getString(resId: Int) : String = NexxasIdAuthApp.appContext.getString(resId)
-
     fun treatFailureMessage(t: Throwable): String {
         return when (t is SocketTimeoutException) {
-            true -> getString(R.string.timeout_error)
-            false -> getString(R.string.unexpected_error)
+            true -> Consts.TIMEOUT_EXCEPTION
+            false -> Consts.UNEXPECTED_ERROR
         }
     }
 }
